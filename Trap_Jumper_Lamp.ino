fce9a613be91e7928aea04a7f5071268b682011e
Gringl <64546287+Gringl@users.noreply.github.com>
2021-09-05 23:50:02 -0700

    Update Trap_Jumper_Lamp.ino
    added back LED controls, also added delay(0) to feed watchdog

--- Trap_Jumper_Lamp.ino
+++ Trap_Jumper_Lamp.ino
@@ -1,9 +1,10 @@
 #include <ESP8266WiFi.h>
 #include <espnow.h>
 #include <FastLED.h>
-#define NUM_LEDS 60
+#define NUM_LEDS 144
 //GPIO6, GPIO7, GPIO8, GPIO9, GPIO10, and GPIO11 are all unavailable
 #define DATA_PIN  2
+
 
 CRGB leds[NUM_LEDS];
 
@@ -15,20 +16,22 @@ struct_message myData;
 
 void OnDataRecv(uint8_t * mac, uint8_t *incomingData, uint8_t len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  Serial.print("Bytes received: ");
-  Serial.println(len);
-  Serial.println("Music Val: ");
   Serial.println(myData.a);
+  fill_rainbow( leds, myData.a, 0, 255/NUM_LEDS);
+  FastLED.show();
+  EVERY_N_MILLISECONDS(10){
+    fadeToBlackBy(leds, NUM_LEDS, 64);
+    FastLED.show();
+    delay(0);
+  }
 }
 
 void setup() {
   // put your setup code here, to run once:
   //Add LEDs
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
-  //fill Strip with rainbow
-  fill_rainbow( leds, NUM_LEDS, 0, 255 / NUM_LEDS );
-  FastLED.show();
   Serial.begin(115200);
+  FastLED.setBrightness(65);
   WiFi.mode(WIFI_STA);//sets wifi mode as a station
   // Init ESP-NOW
   if (esp_now_init() != 0) {
@@ -40,6 +43,5 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // put your main code here, to run repeatedly: 
 }
